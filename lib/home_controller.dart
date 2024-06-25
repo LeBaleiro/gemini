a84a74d29f0b986cb983e45fef16c1d563c826d2
LeBaleiro <lehbaleiro@gmail.com>
2024-06-25 15:21:52 -0300

    wip

--- lib/home_controller.dart
+++ lib/home_controller.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:gemini/shared/computed_notifier.dart';
 import 'package:gemini/states/image_states.dart';
 import 'package:gemini/states/request_states.dart';
 import 'package:gemini/stores/image_store.dart';
 import 'package:gemini/stores/request_store.dart';
+import 'package:value_selectable/value_selectable.dart';
 
 class HomeController {
   HomeController(this._$requestStore, this._$imageStore);
@@ -17,20 +17,19 @@ class HomeController {
 
   late final $textController = TextEditingController();
 
-  late final $envioDeImagensHabilitado = MultiNotifier<bool>(
-    valueListenables: [$envioTextHabilitado, _$imageStore],
-    compute: () {
-      final imagensSelecionadas = _$imageStore.value is ImageSuccessState;
-      return imagensSelecionadas && $envioTextHabilitado.value;
-    },
+  late final $envioDeImagensHabilitado = ValueSelector<bool>(
+    (get) => get($imagensSelecionadas) && get($envioTextHabilitado),
   );
 
-  late final $envioTextHabilitado = MultiNotifier<bool>(
-    valueListenables: [_$requestStore, $textController],
-    compute: () {
-      final requestEmProgresso = _$requestStore.value is RequestLoadingState;
-      return $textController.value.text.isNotEmpty && !requestEmProgresso;
-    },
+  late final $imagensSelecionadas =
+      ValueSelector<bool>((get) => get(_$imageStore) is ImageSuccessState);
+
+  late final $envioTextHabilitado = ValueSelector<bool>(
+    (get) => get($textController).text.isNotEmpty && !get($requestEmProgresso),
+  );
+
+  late final $requestEmProgresso = ValueSelector<bool>(
+    (get) => get($requestStore) is RequestLoadingState,
   );
 
   Future<void> escolherImagens() => _$imageStore.escolherImagens();
