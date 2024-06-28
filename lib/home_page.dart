a2a7d8f92a49fb1eee9c2c0329dc022fb76a3925
LeBaleiro <lehbaleiro@gmail.com>
2024-06-28 16:25:27 -0300

    wip

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:gemini/widgets/imagem_list.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -14,14 +15,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String? modelResponse;
 
-  late final controller = TextEditingController();
+  late final textController = TextEditingController();
 
   late final imagePicker = ImagePicker();
   List<XFile>? imagens;
 
+  bool selecionandoImagem = false;
+  String? erroImagem;
   Future<void> escolherImagens() async {
-    final result = await imagePicker.pickMultiImage();
-    setState(() => imagens = result);
+    setState(() {
+      selecionandoImagem = true;
+      erroImagem = null;
+    });
+    try {
+      final result = await imagePicker.pickMultiImage();
+      setState(() {
+        imagens = result;
+        selecionandoImagem = false;
+      });
+    } catch (e) {
+      setState(() {
+        erroImagem = e.toString();
+        selecionandoImagem = false;
+      });
+    }
   }
 
   late final model = GenerativeModel(
@@ -29,22 +46,53 @@ class _HomePageState extends State<HomePage> {
     apiKey: const String.fromEnvironment('API_KEY'),
   );
 
+  bool enviandoDadosParaGemini = false;
+  String? erroGemini;
+
   Future<void> enviarTexto(String text) async {
-    final content = [Content.text(text)];
-    final response = await model.generateContent(content);
-    setState(() => modelResponse = response.text);
+    setState(() {
+      enviandoDadosParaGemini = true;
+      erroGemini = null;
+    });
+    try {
+      final content = [Content.text(text)];
+      final response = await model.generateContent(content);
+      setState(() {
+        modelResponse = response.text;
+        enviandoDadosParaGemini = false;
+      });
+    } catch (e) {
+      setState(() {
+        erroGemini = e.toString();
+        enviandoDadosParaGemini = false;
+      });
+    }
   }
 
   Future<void> enviarImagens(String text) async {
+    setState(() {
+      enviandoDadosParaGemini = true;
+      erroGemini = null;
+    });
     final prompt = TextPart(text);
     if (imagens == null) return;
-    final fileImages =
-        await imagens!.map((e) => File(e.path).readAsBytes()).wait;
-    final imageParts = fileImages.map((e) => DataPart('image/png', e));
-    final response = await model.generateContent([
-      Content.multi([prompt, ...imageParts])
-    ]);
-    setState(() => modelResponse = response.text);
+    try {
+      final fileImages =
+          await imagens!.map((e) => File(e.path).readAsBytes()).wait;
+      final imageParts = fileImages.map((e) => DataPart('image/png', e));
+      final response = await model.generateContent([
+        Content.multi([prompt, ...imageParts])
+      ]);
+      setState(() {
+        modelResponse = response.text;
+        enviandoDadosParaGemini = false;
+      });
+    } catch (e) {
+      setState(() {
+        erroGemini = e.toString();
+        enviandoDadosParaGemini = false;
+      });
+    }
   }
 
   @override
@@ -56,38 +104,58 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(controller: controller),
+              TextField(controller: textController),
+              const SizedBox(height: 10),
+              ValueListenableBuilder(
+                  valueListenable: textController,
+                  builder: (context, value, child) {
+                    final campoVazio = value.text.isEmpty;
+                    final botaoDesabilitado =
+                        enviandoDadosParaGemini || campoVazio;
+                    return TextButton(
+                      onPressed: botaoDesabilitado
+                          ? null
+                          : () => enviarTexto(textController.text),
+                      child: const Text('Enviar texto'),
+                    );
+                  }),
               const SizedBox(height: 10),
               TextButton(
-                onPressed: () => enviarTexto(controller.text),
-                child: const Text('Enviar texto'),
-              ),
-              const SizedBox(height: 10),
-              TextButton(
-                onPressed: escolherImagens,
+                onPressed: selecionandoImagem ? null : escolherImagens,
                 child: const Text('Escolher imagens'),
               ),
               const SizedBox(height: 10),
-              if (imagens != null)
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: imagens!
-                      .map(
-                        (e) => Image.file(
-                          File(e.path),
-                          height: 200,
-                          fit: BoxFit.fitHeight,
-                        ),
-                      )
-                      .toList(),
-                ),
+              if (erroImagem != null) ...{
+                Text(erroImagem!)
+              } else if (imagens != null) ...{
+                ImagemList(imagens: imagens)
+              } else if (selecionandoImagem) ...{
+                const CircularProgressIndicator()
+              },
               const SizedBox(height: 10),
-              TextButton(
-                onPressed: () => enviarImagens(controller.text),
-                child: const Text('Enviar imagens'),
-              ),
+              ValueListenableBuilder(
+                  valueListenable: textController,
+                  builder: (context, value, child) {
+                    final campoVazio = value.text.isEmpty;
+                    final imagemSelecionada = imagens != null;
+                    final botaoDesabilitado = enviandoDadosParaGemini ||
+                        !imagemSelecionada ||
+                        campoVazio;
+                    return TextButton(
+                      onPressed: botaoDesabilitado
+                          ? null
+                          : () => enviarImagens(textController.text),
+                      child: const Text('Enviar imagens'),
+                    );
+                  }),
               const SizedBox(height: 10),
-              Text(modelResponse ?? 'Sem resposta'),
+              if (enviandoDadosParaGemini) ...{
+                const CircularProgressIndicator()
+              } else if (erroGemini != null) ...{
+                Text(erroGemini!),
+              } else ...{
+                Text(modelResponse ?? 'Sem resposta'),
+              },
             ],
           ),
         ),
